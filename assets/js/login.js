a57520776ba79510f732a30d193758ffc09b0e1f
xiongchencheng <1171755947@qq.com>
2020-06-27 19:24:23 +0800

    更新2.0

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,14 +1,14 @@
 $(function () {
     // 点击去注册按钮
     $('#link_reg').on('click', function () {
-        $('.login_box').show()
-        $('.reg_box').hide()
+        $('.login_box').hide()
+        $('.reg_box').show()
     })
 
     // 点击去登陆按钮
     $('#link_login').on('click', function () {
-        $('.login_box').hide()
-        $('.reg_box').show()
+        $('.login_box').show()
+        $('.reg_box').hide()
     })
 
     // 密码的验证规则
@@ -16,16 +16,16 @@ $(function () {
     const layer = layui.layer
     form.verify({
         // 自定义了一个叫做 pwd 校验规则
-        pwd:[/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'],
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
         // 校验两次密码是否一致的规则
         repwd: function (value) {
             // 通过形参拿到的是确认密码框中的内容
             // 还需要拿到密码框中的内容
             // 然后进行一次等于的判断
             // 如果判断失败,则return一个提示消息即可
-            var pwd = $('.reg-box [name=password]').val()
+            var pwd = $('.reg_box [name=password]').val()
             if (pwd !== value) {
-                return '两次密码不一致！'
+                return '两次密码不一致'
             }
         }
     })
@@ -33,14 +33,15 @@ $(function () {
     $('#form_reg').on('submit', function (e) {
         e.preventDefault()
         const inputParams = {
-            username: $('#form_reg [username=username]').val(),
-            password: $('#form_reg [password=password]').val()
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
         }
-        $.post('http://127.0.0.1:3007/api/reguser', inputParams, function (res) {
+        $.post('/api/reguser', inputParams, function (res) {
             if (res.status !== 0) {
                 return layer.msg(res.message)
             }
             layer.msg('注册成功，请登录')
+            $('#link_login').click()
         })
     })
     // 监听登陆点击事件
@@ -48,14 +49,15 @@ $(function () {
         e.preventDefault()
         $.ajax({
             type: 'POST',
-            url: 'http://127.0.0.1:3007/api/login',
-            data: '$(this).serialize()',
+            url: '/api/login',
+            data: $(this).serialize(),
             success: function (res) {
                 if (res.status !== 0) {
-                    return layer.msg('res')
+                    return layer.msg(res.message)
                 }
+                layer.msg('登陆成功')
                 localStorage.setItem('token', res.token)
-                location.href = '/index.html'
+                location.href = '/bigNews/index.html'
             }
 
         })
